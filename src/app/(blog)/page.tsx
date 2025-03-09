62ca478247b438786c38ee3f4ceccc7c4a0fb3e3
WashingtonDDS <washington.DDS@hotmail.com>
2025-03-09 19:49:18 -0300

    fetch posts data and update Blog component to render posts dynamically

--- src/app/(blog)/page.tsx
+++ src/app/(blog)/page.tsx
@@ -1,3 +1,4 @@
+import { TPost } from "@/types/typeBlog";
 import { createRequest } from "@/utils/createRequest";
 import Link from "next/link";
 
@@ -5,27 +6,31 @@ const getData = async () => {
   const posts = await createRequest("/posts", {
     method: "GET",
   });
+  return posts;
 };
 
-export default function Blog() {
+export default async function Blog() {
+  const posts: TPost[] = await getData();
   return (
     <div className="py-6 lg:py-12">
       <div className="grid gap-6 px-4 mx-auto md:grid-cols-2 md:px-6 lg:grid-cols-2xl max-w-6xl">
-        <div className="space-y-2">
-          <Link className="font-medium" href="#">
-            <h2 className="text-2xl font-bold tracking-tight leading-6">
-              The Art of the Perfect Cup: A Coffee Lover's Guide
-            </h2>
-          </Link>
-          <p className="text-gray-500 dark:text-gray-400">
-            Posted on August 24, 2023
-          </p>
-          <p>
-            For many people, the day doesn't truly start until they've had their
-            first cup of coffee. Whether it's a rich and velvety latte, a bold
-            and...
-          </p>
-        </div>
+        {posts.map((item) => (
+          <div className="space-y-2" key={item.id}>
+            <Link className="font-medium" href="#">
+              <h2 className="text-2xl font-bold tracking-tight leading-6">
+                The Art of the Perfect Cup: A Coffee Lover's Guide
+              </h2>
+            </Link>
+            <p className="text-gray-500 dark:text-gray-400">
+              Posted on August 24, 2023
+            </p>
+            <p>
+              For many people, the day doesn't truly start until they've had
+              their first cup of coffee. Whether it's a rich and velvety latte,
+              a bold and...
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
