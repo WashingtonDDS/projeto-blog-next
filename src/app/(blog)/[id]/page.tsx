885092c518b53e02d7a81f1b0fb8d324a6c7b986
WashingtonDDS <washington.DDS@hotmail.com>
2025-03-09 20:22:16 -0300

    fetch post data in Post component and render dynamically

--- src/app/(blog)/[id]/page.tsx
+++ src/app/(blog)/[id]/page.tsx
@@ -1,17 +1,19 @@
 import { TPostProps } from "@/types/typeBlog";
+import { createRequest } from "@/utils/createRequest";
 
-const getPost = async (id: string) => {};
+const getPost = async (id: string) => {
+  const post = await createRequest(`/posts/${id}`, {
+    method: "GET",
+  });
+  return post;
+};
 
-export default function Post({ params }: TPostProps) {
+export default async function Post({ params }: TPostProps) {
+  const post = await getPost(params.id);
   return (
     <div className="py-6 px-6 lg:py-12 text-center max-w-6xl mx-auto">
-      <h1 className="text-2xl font-bold tracking-tight">Lorem ipsum</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quaerat
-        reiciendis saepe nostrum, deserunt eveniet rem fugit odit quia debitis
-        architecto amet sit excepturi in ex consequatur inventore nisi
-        veritatis.
-      </p>
+      <h1 className="text-2xl font-bold tracking-tight">{post.title}</h1>
+      <p>{post.content}</p>
     </div>
   );
 }
