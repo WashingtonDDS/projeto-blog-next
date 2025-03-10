aae2e9191a3557c5c9bc2c0be4f192eebdae92ae
WashingtonDDS <washington.DDS@hotmail.com>
2025-03-10 19:32:41 -0300

    add createRequestAuth function for authenticated API requests

--- src/utils/createRequest.ts
+++ src/utils/createRequest.ts
@@ -5,3 +5,8 @@ export async function createRequest(url: string, initialValues: object) {
   const response = await fetch(`${baseUrl}${url}`, initialValues);
   return await response.json();
 }
+
+export async function createRequestAuth(url: string, initialValues: object) {
+  const response = await fetch(`${baseUrlAuth}${url}`, initialValues);
+  return await response.json();
+}
