f1bb6c0fee3a3e313cbef06f4d9696795d398593
WashingtonDDS <washington.DDS@hotmail.com>
2025-03-11 21:37:45 -0300

    refactor SignIn component to handle form input and integrate createRequestAuth for login

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,9 +1,28 @@
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { ChangeEvent, FormEvent, useState } from "react";
+import { createRequestAuth } from "@/utils/createRequest";
 export default function SignIn() {
+  const handleFormSignUp = async (event:FormEvent)=>{
+    const [form, setForm] = useState()
+
+    const handleChangeInput = (event:ChangeEvent<HTMLInputElement>)=>{
+      const {name, value} = event.target
+      setForm(prevForm => {
+        return {
+          ...prevForm,
+          [name]:value,
+        }
+      })
+    }
+
+    await createRequestAuth ('/login',{
+      method: 'POST',
+      body: JSON.stringify(form)
+  }
   return (
-    <form className="px-4 py-12 space-y-12 md:py-24 md:px-6">
+    <form onSubmit={handleFormSignin} className="px-4 py-12 space-y-12 md:py-24 md:px-6">
       <div className="mx-auto max-w-sm space-y-6">
         <div className="space-y-2 text-center">
           <h1 className="text-3xl font-bold">Login</h1>
