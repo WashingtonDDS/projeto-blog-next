2c0e42641676279dba36fc566e6c90cd4d5a0b08
WashingtonDDS <washington.DDS@hotmail.com>
2025-03-11 21:57:40 -0300

    refactor SignIn component to manage form state and handle login requests with improved type definitions

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,32 +1,49 @@
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { createRequestAuth } from "@/utils/createRequest";
+import { TForm } from "@/types/typeSign";
 export default function SignIn() {
-  const handleFormSignUp = async (event:FormEvent)=>{
-    const [form, setForm] = useState()
+  const [form, setForm] = useState<TForm>({
+    email: "",
+    password: "",
+  });
 
-    const handleChangeInput = (event:ChangeEvent<HTMLInputElement>)=>{
-      const {name, value} = event.target
-      setForm(prevForm => {
-        return {
-          ...prevForm,
-          [name]:value,
-        }
-      })
-    }
+  const handleChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setForm((prevForm) => {
+      return {
+        ...prevForm,
+        [name]: value,
+      };
+    });
+  };
 
-    await createRequestAuth ('/login',{
-      method: 'POST',
-      body: JSON.stringify(form)
-  }
+  const handleFormSignin = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!form.email || !form.password) return;
+
+    await createRequestAuth("/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: form.email,
+        senha: form.password,
+      }),
+    });
+  };
   return (
-    <form onSubmit={handleFormSignin} className="px-4 py-12 space-y-12 md:py-24 md:px-6">
+    <form
+      onSubmit={handleFormSignin}
+      className="px-4 py-12 space-y-12 md:py-24 md:px-6"
+    >
       <div className="mx-auto max-w-sm space-y-6">
         <div className="space-y-2 text-center">
           <h1 className="text-3xl font-bold">Login</h1>
-          <p className="text-gray-500 dark:text-gray-400">Faça o login para acessar suas transações</p>
+          <p className="text-gray-500 dark:text-gray-400">
+            Faça o login para acessar suas transações
+          </p>
         </div>
         <div className="space-y-2">
           <Label htmlFor="email">Email</Label>
@@ -39,11 +56,7 @@ export default function SignIn() {
         </div>
         <div className="space-y-2">
           <Label htmlFor="password">Senha</Label>
-          <Input
-            id="password"
-            type="password"
-            name="password"
-          />
+          <Input id="password" type="password" name="password" />
         </div>
         <Button className="w-full">Entrar</Button>
       </div>
