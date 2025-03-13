160fc9f407d798fd6a0587c5200d761ce4a08a95
WashingtonDDS <washington.DDS@hotmail.com>
2025-03-12 21:48:51 -0300

    feat: initiate logout functionality

--- src/components/dash/Header.tsx
+++ src/components/dash/Header.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
-
+import Cookies from "js-cookie";
 export function Header() {
+  const logout = () => {
+    Cookies.remove("token");
+    Cookies.remove("user");
+  };
   return (
     <div className="space-y-2">
       <h1 className="text-3xl font-bold lg:text-4xl xl:text-5xl flex justify-between">
